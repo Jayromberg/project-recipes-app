1c449243d4539aab7b694d81e0d9fbe9cfe2bca6
Jayromberg <jayro.dev@gmail.com>
2022-07-14 23:09:56 -0300

    Merge com os testes da Fernanda

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,24 @@
 import React from 'react';
+import profileIcon from '../images/profileIcon.svg';
+import searchIcon from '../images/searchIcon.svg';
 
 function Header() {
   return (
     <div>
-      Heder
+      <object
+        type="image/svg+xml"
+        data={ profileIcon }
+        data-testid="profile-top-btn"
+      >
+        profile
+      </object>
+      <object
+        type="image/svg+xml"
+        data={ searchIcon }
+        data-testid="search-top-btn"
+      >
+        search
+      </object>
     </div>
   );
 }
