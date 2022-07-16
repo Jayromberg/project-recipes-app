564726ffc5249181ca3b74391f21b119be254f38
Fernanda Grein <fernandagrein.fgm@outlook.com>
2022-07-16 19:27:39 -0300

    adiciona correções dos requisitos

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,39 +1,57 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useHistory } from 'react-router-dom';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
 
 function Profile() {
   const history = useHistory();
-  const { email } = JSON.parse(localStorage.getItem('user'));
+  const [userEmail, setUserEmail] = useState('erro');
+
+  useEffect(() => {
+    const getFromLocal = JSON.parse(localStorage.getItem('user'));
+    if (getFromLocal) {
+      const { email } = getFromLocal;
+      setUserEmail(email);
+    }
+  }, []);
+
   return (
     <div>
       <Header />
-      <p data-testid="profile-email">{email}</p>
-      <button
-        data-testid="profile-done-btn"
-        type="button"
-        onClick={ () => { history.push('./done-recipes'); } }
-      >
-        Done Recipes
-      </button>
-      <button
-        data-testid="profile-favorite-btn"
-        type="button"
-        onClick={ () => { history.push('./favorite-recipes'); } }
-      >
-        Favorite Recipes
-      </button>
-      <button
-        data-testid="profile-logout-btn"
-        type="button"
-        onClick={ () => {
-          history.push('./');
-          localStorage.clear();
-        } }
-      >
-        Logout
-      </button>
+      <section>
+        <p data-testid="profile-email">{userEmail}</p>
+        <button
+          data-testid="profile-done-btn"
+          type="button"
+          onClick={ () => { history.push('./done-recipes'); } }
+        >
+          Done Recipes
+        </button>
+        <button
+          data-testid="profile-favorite-btn"
+          type="button"
+          onClick={ () => { history.push('./favorite-recipes'); } }
+        >
+          Favorite Recipes
+        </button>
+        <button
+          data-testid="profile-logout-btn"
+          type="button"
+          onClick={ () => {
+            history.push('./');
+            localStorage.clear();
+          } }
+        >
+          Logout
+        </button>
+      </section>
+      <p>
+        <br />
+        {`  
+          
+          `}
+        <br />
+      </p>
       <Footer />
     </div>
   );
