3a76341fd7a1c8dcef37d0466f1f0ed20e153757
Fernanda Grein <fernandagrein.fgm@outlook.com>
2022-07-19 17:42:31 -0300

    corrige lint

--- src/test/Login.test.js
+++ src/test/Login.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { screen, waitFor } from '@testing-library/react';
+import { screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import renderWithRouter from './renderWithRouter';
 import App from '../App';
--- src/test/Rotas.test.js
+++ src/test/Rotas.test.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { screen, waitFor } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import renderWithRouter from './renderWithRouter';
-import meals from '../../cypress/mocks/meals';
 import drinks from '../../cypress/mocks/drinks';
 import App from '../App';
 // import oneMeal from '../../cypress/mocks/oneMeal';
@@ -10,31 +9,26 @@ import oneDrink from '../../cypress/mocks/oneDrink';
 // import oneDrinkId15997 from '../../cypress/mocks/oneDrinkId15997';
 import acbDrinkData from './acbMock';
 import poutineData from './poutineMock';
+import meals from '../../cypress/mocks/meals';
 
 describe('testa as rotas dentro do componente App', () => {
   beforeEach(() => {
     jest.spyOn(global, 'fetch').mockImplementation(async (url) => ({
       json: async () => {
-        if (url === 'https://www.themealdb.com/api/json/v1/1/search.php?s=') {
-          return meals;
-        }
         if (url === 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=') {
           return drinks;
         }
-        // if (url === 'https://www.themealdb.com/api/json/v1/1/lookup.php?i=52771') {
-        //   return oneMeal;
-        // }
         if (url === 'https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=178319') {
           return oneDrink;
-        } if (url === 'https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=13501') {
-          return acbDrinkData;
-        }
-        if (url === 'https://www.themealdb.com/api/json/v1/1/lookup.php?i=52804') {
-          return poutineData;
         }
       },
     }));
+
+    const ALERT = 'Sorry, we haven\'t found any recipes for these filters.';
+    jest.spyOn(global, 'alert')
+      .mockImplementation(() => ALERT);
   });
+
   afterEach(() => {
     global.fetch.mockRestore();
   });
@@ -57,13 +51,37 @@ describe('testa as rotas dentro do componente App', () => {
     // console.log(buttonEl);
     // console.log(history);
 
-    // await waitFor(() => {
-    //   expect(screen.getByRole('heading', { name: /Aquamarine/i }))
-    //     .toBeInTheDocument();
-    // });
+    await waitFor(() => {
+      expect(screen.getByRole('heading', { name: /Aquamarine/i }))
+        .toBeInTheDocument();
+    });
+  });
+});
+
+describe('testa as rotas da página de comidas e bebidas para a tela de detalhe', () => {
+  beforeEach(() => {
+    jest.spyOn(global, 'fetch').mockImplementation(async (url) => ({
+      json: async () => {
+        if (url === 'https://www.themealdb.com/api/json/v1/1/search.php?s=') {
+          return meals;
+        }
+        if (url === 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=') {
+          return drinks;
+        }
+        if (url === 'https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=13501') {
+          return acbDrinkData;
+        }
+        if (url === 'https://www.themealdb.com/api/json/v1/1/lookup.php?i=52804') {
+          return poutineData;
+        }
+      },
+    }));
+  });
+  afterEach(() => {
+    global.fetch.mockRestore();
   });
   it(`testa se ao clicar em uma receita ela redireciona 
-  para a página de detalhes da receita`, async () => {
+    para a página de detalhes da receita`, async () => {
     const { history } = renderWithRouter(<App />);
     history.push('/foods');
     const foodEl = await screen.findByText(/poutine/i);
@@ -76,7 +94,7 @@ describe('testa as rotas dentro do componente App', () => {
     });
   });
   it(`testa se ao clicar em uma receita ela redireciona 
-  para a página de detalhes da receita`, async () => {
+    para a página de detalhes da receita`, async () => {
     const { history } = renderWithRouter(<App />);
     history.push('/drinks');
     const drinlEl = await screen.findByText(/abc/i);
