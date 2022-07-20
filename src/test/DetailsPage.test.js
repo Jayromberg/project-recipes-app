7ee5e7f742e88fb92ac881a76d2494ce8bb32540
Jayromberg <jayro.dev@gmail.com>
2022-07-20 20:20:55 -0300

    Testes do requisito 36

--- src/test/DetailsPage.test.js
+++ src/test/DetailsPage.test.js
@@ -3,17 +3,21 @@ import { screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import renderWithRouter from './renderWithRouter';
 import drinks from '../../cypress/mocks/drinks';
+import meals from '../../cypress/mocks/meals';
 import acbDrinkData from './acbMock';
 import drinkCategories from '../../cypress/mocks/drinkCategories';
 import App from '../App';
 // import RecipeDetails, { drinkRecipes } from '../components/RecipeDetails';
 
 describe('testa a página de detalhes', () => {
-  beforeEach(() => {
+  beforeEach(async () => {
     jest.spyOn(global, 'fetch').mockImplementation(async (url) => ({
       json: async () => {
         if (url === 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=') {
           return drinks;
+        }
+        if (url === 'https://www.themealdb.com/api/json/v1/1/search.php?s=') {
+          return meals;
         }
         if (url === 'https://www.thecocktaildb.com/api/json/v1/1/list.php?c=list') {
           return drinkCategories;
@@ -23,7 +27,8 @@ describe('testa a página de detalhes', () => {
         }
       },
     }));
-    const { history } = renderWithRouter(<App />);
+
+    const { history } = await renderWithRouter(<App />);
     history.push('/drinks');
   });
   afterEach(() => {
@@ -53,8 +58,21 @@ describe('testa a página de detalhes', () => {
     // expect(BurekRecomendation).toBeInTheDocument();
   });
   it('testa o card de recomendações', async () => {
-    // const drinkEl = await screen.findByText(/abc/i);
-    // userEvent.click(drinkEl);
+    const drinkEl = await screen.findByText(/abc/i);
+    userEvent.click(drinkEl);
+
+    const recomedantion0 = await screen.findByTestId('0-recomendation-card');
+    const recomedantion1 = screen.getByTestId('1-recomendation-card');
+    const recomedantion2 = screen.queryByTestId('2-recomendation-card');
+    const recomedantion3 = screen.getByTestId('3-recomendation-card');
+    const recomedantion4 = screen.getByTestId('4-recomendation-card');
+    const recomedantion5 = screen.getByTestId('5-recomendation-card');
+    expect(recomedantion0).toBeInTheDocument();
+    expect(recomedantion1).toBeInTheDocument();
+    expect(recomedantion2).toBeInTheDocument();
+    expect(recomedantion3).toBeInTheDocument();
+    expect(recomedantion4).toBeInTheDocument();
+    expect(recomedantion5).toBeInTheDocument();
 
     // const card = document.createElement('div');
     // card.scrollBy = jest.fn();
--- src/test/renderWithRouter.js
+++ src/test/renderWithRouter.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import { Router } from 'react-router-dom';
+import { act } from 'react-dom/test-utils';
 import { createMemoryHistory } from 'history';
 
-const renderWithRouter = (component) => {
+const renderWithRouter = async (component) => {
   const history = createMemoryHistory();
 
-  const returnFromRender = render(
+  const returnFromRender = await act(async () => (render(
     <Router history={ history }>
       {component}
     </Router>,
-  );
+  )));
 
   return { ...returnFromRender, history };
 };
