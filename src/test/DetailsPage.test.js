f4eb91a823fdecebabf3ce335708622bd0c2b411
Fernanda Grein <fernandagrein.fgm@outlook.com>
2022-07-19 21:23:45 -0300

    inicia teste do details Page

---
+++ src/test/DetailsPage.test.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from './renderWithRouter';
+import drinks from '../../cypress/mocks/drinks';
+import acbDrinkData from './acbMock';
+import drinkCategories from '../../cypress/mocks/drinkCategories';
+import App from '../App';
+
+describe('testa a página de detalhes', () => {
+  beforeEach(() => {
+    jest.spyOn(global, 'fetch').mockImplementation(async (url) => ({
+      json: async () => {
+        if (url === 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=') {
+          return drinks;
+        }
+        if (url === 'https://www.thecocktaildb.com/api/json/v1/1/list.php?c=list') {
+          return drinkCategories;
+        }
+        if (url === 'https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=13501') {
+          return acbDrinkData;
+        }
+      },
+    }));
+    const { history } = renderWithRouter(<App />);
+    history.push('/drinks');
+  });
+  afterEach(() => {
+    global.fetch.mockRestore();
+  });
+  it('testa os elementos da página de detalhe', async () => {
+    const drinkEl = await screen.findByText(/abc/i);
+    userEvent.click(drinkEl);
+
+    const ingredient01 = await screen.findByText('Amaretto 1/3');
+    const ingredient02 = await screen.findByText('Baileys irish cream 1/3');
+    const ingredient03 = await screen.findByText('Cognac 1/3');
+    const recipe = await screen.findByText('Layered in a shot glass.');
+
+    expect(ingredient01).toBeInTheDocument();
+    expect(ingredient02).toBeInTheDocument();
+    expect(ingredient03).toBeInTheDocument();
+    expect(recipe).toBeInTheDocument();
+
+    // const corbaRecomendation = await screen.findByText('corba');
+    // const BurekRecomendation = await screen.findByText('burek');
+
+    // expect(corbaRecomendation).toBeInTheDocument();
+    // expect(BurekRecomendation).toBeInTheDocument();
+  });
+});
