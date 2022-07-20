9e4940a3f64344adc78c9d372b9620d0043a2e10
Jayromberg <jayro.dev@gmail.com>
2022-07-20 13:44:10 -0300

    Requisito 34

--- src/components/ShareButton.jsx
+++ src/components/ShareButton.jsx
@@ -1,14 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useHistory } from 'react-router-dom/cjs/react-router-dom';
+import shareIcon from '../images/shareIcon.svg';
+
+const copy = require('clipboard-copy');
 
 function ShareButton() {
+  const history = useHistory();
+  const [wasCopied, setWasCopied] = useState(false);
+
+  function copyLink() {
+    const { pathname } = history.location;
+    setWasCopied(true);
+    copy(`http://localhost:3000${pathname}`);
+  }
+
   return (
     <div>
       <button
         type="button"
         data-testid="share-btn"
+        onClick={ copyLink }
       >
-        Share
+        <img src={ shareIcon } alt="Share Button" />
       </button>
+      {wasCopied && (
+        <p>Link copied!</p>
+      )}
     </div>
   );
 }
